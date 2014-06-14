1004706f1f146e58d2859392b3ae901b4bcf4147
SolarLiner <solarliner@gmail.com>
2014-06-14 16:16:40 +0200

    Separated File and created kFunc classes
    kFunc will be used for storing Function informations for later use in
    the IDE.
    File handles any I/O on script files.

---
+++ File.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kOS_IDE
+{
+    class File
+    {
+        /// <summary>
+        /// File name of the script.
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Unstripped file name of the script, if found (returns string.Empty if not).
+        /// </summary>
+        public string stripFName { get; private set; }
+
+        /// <summary>
+        /// Check if the script is stripped by checking a strip file corresponding.
+        /// </summary>
+        public bool IsStripped { get { return String.IsNullOrWhiteSpace(stripFName); } }
+
+        /// <summary>
+        /// Check if the script is a function by scanning through the file for "declare parameter".
+        /// </summary>
+        public bool IsFunction { get { return Function == null; } }
+
+        /// <summary>
+        /// Pointer to the function.
+        /// </summary>
+        public kFunc Function { get; private set; }
+
+        /// <summary>
+        /// Global initializer.
+        /// </summary>
+        /// <param name="FileName">FileName input (full path).</param>
+        public File(string FileName)
+        {
+
+        }
+    }
+}
---
+++ kFunc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace kOS_IDE
+{
+    class kFunc
+    {
+        /// <summary>
+        /// Array of parameters.
+        /// </summary>
+        public string[] Parameters { get; private set; }
+
+        /// <summary>
+        /// Global initializer.
+        /// </summary>
+        /// <param name="parameters">Inputs the parameters of the function.</param>
+        public kFunc(params string[] parameters)
+        {
+            Parameters = parameters;
+        }
+    }
+}
